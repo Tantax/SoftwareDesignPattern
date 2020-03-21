62f35f2d817e4ec21d44031ee350a0a25575b9b0
Birch Tang <tantax-tang@foxmail.com>
2020-03-21 14:29:06 +0800

    原型模式

---
+++ SoftwareDesignPattern/创建型模式/原型模式/Prototype.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftwareDesignPattern
+{
+    /// <summary>
+    /// 原型模式
+    /// </summary>
+    public class Prototype
+    {
+
+    }
+}
