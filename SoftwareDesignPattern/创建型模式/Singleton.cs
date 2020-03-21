e9d5a8c90737e1424040b615ff4415b49e372d4a
Berg Tang <tantax-tang@foxmail.com>
2020-03-21 11:13:10 +0800

    单例模式

---
+++ SoftwareDesignPattern/创建型模式/Singleton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftwareDesignPattern
+{
+    /// <summary>
+    /// 单例模式
+    /// </summary>
+    public class Singleton
+    {
+        private static readonly object _lockObj = new object();
+        private Singleton() { }
+
+
+        #region 懒汉式单例
+        private static Singleton _obj;
+        public static Singleton GetLazyInstance()
+        {
+            lock (_lockObj)
+            {
+                if (_obj == null)
+                {
+                    _obj = new Singleton();
+                }
+            }
+            return _obj;
+        }
+        #endregion
+
+        #region 饿汉式单例
+        public static readonly Singleton _obj2 = new Singleton();
+        public static Singleton GetHungryInstance()
+        {
+            return _obj2;
+        } 
+        #endregion
+    }
+
+
+
+}
