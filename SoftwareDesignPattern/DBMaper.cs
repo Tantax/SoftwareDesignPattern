6f16828dcfc4e1cc18459b3de2b5c563e6327a19
Birch Tang <tantax-tang@foxmail.com>
2020-03-21 15:28:04 +0800

    原型模式

---
+++ SoftwareDesignPattern/DBMaper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftwareDesignPattern
+{
+    public static class DBMaper
+    {
+        public static T Copy<T>(this object source)
+            where T : new()
+        {
+            if (source == null)
+                return default;
+
+            T re = new T();
+            var pList = typeof(T).GetProperties();
+            var sType = source.GetType();
+            foreach (var pInfo in pList)
+            {
+                var sInfo = sType.GetProperty(pInfo.Name);
+                if (sInfo != null)
+                {
+                    pInfo.SetValue(re, sInfo.GetValue(source));
+                }
+            }
+
+            return re;
+        }
+    }
+}
--- SoftwareDesignPattern/创建型模式/原型模式/Prototype.cs
+++ SoftwareDesignPattern/创建型模式/原型模式/Prototype.cs
@@ -7,8 +7,12 @@ namespace SoftwareDesignPattern
     /// <summary>
     /// 原型模式
     /// </summary>
-    public class Prototype
+    public class Prototype : ICloneable
     {
-
+        public object Clone()
+        {
+            //return this.MemberwiseClone();// 浅拷贝
+            return this.Copy<Prototype>();// 深拷贝
+        }
     }
 }
