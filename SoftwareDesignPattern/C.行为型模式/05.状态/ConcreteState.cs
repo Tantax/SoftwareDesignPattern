806d938a185e725a9e1c6ec01bdc6e27f830a2a9
Birch Tang <tantax-tang@foxmail.com>
2020-04-18 16:37:02 +0800

    状态模式

---
+++ SoftwareDesignPattern/C.行为型模式/05.状态/ConcreteState.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftwareDesignPattern.State
+{
+    class ConcreteStateA : State
+    {
+        public override void Handle(Context context)
+        {
+            Console.WriteLine("当前状态是 A.");
+            context.State = new ConcreteStateB();
+        }
+    }
+
+    class ConcreteStateB : State
+    {
+        public override void Handle(Context context)
+        {
+            Console.WriteLine("当前状态是 B.");
+            context.State = new ConcreteStateA();
+        }
+    }
+}
---
+++ SoftwareDesignPattern/C.行为型模式/05.状态/Context.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftwareDesignPattern.State
+{
+    class Context
+    {
+        public Context()
+        {
+            State = new ConcreteStateA();
+        }
+
+        public State State { get; set; }
+
+
+        public void Handle()
+        {
+            State.Handle(this);
+        }
+    }
+}
