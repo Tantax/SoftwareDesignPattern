434725ef92154a0e719ddb8f5b45789d7cfde9c1
Birch Tang <tantax-tang@foxmail.com>
2020-04-18 16:20:41 +0800

    责任链模式

---
+++ SoftwareDesignPattern/C.行为型模式/04.职责链/ConcreteHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftwareDesignPattern.ChainOfResponsibility
+{
+    class ConcreteHandler1 : Handler
+    {
+        public override void HandleRequest(string request)
+        {
+            if (request.Equals("one"))
+            {
+                Console.WriteLine("具体处理者1负责处理该请求！");
+            }
+            else
+            {
+                if (GetNext() != null)
+                {
+                    GetNext().HandleRequest(request);
+                }
+                else
+                {
+                    Console.WriteLine("没有人处理该请求！");
+                }
+            }
+        }
+    }
+
+    class ConcreteHandler2 : Handler
+    {
+        public override void HandleRequest(string request)
+        {
+            if (request.Equals("two"))
+            {
+                Console.WriteLine("具体处理者2负责处理该请求！");
+            }
+            else
+            {
+                if (GetNext() != null)
+                {
+                    GetNext().HandleRequest(request);
+                }
+                else
+                {
+                    Console.WriteLine("没有人处理该请求！");
+                }
+            }
+        }
+    }
+}
---
+++ SoftwareDesignPattern/C.行为型模式/04.职责链/Handler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftwareDesignPattern.ChainOfResponsibility
+{
+    abstract class Handler
+    {
+        private Handler next = null;
+
+
+        public void SetNext(Handler next)
+        {
+            this.next = next;
+        }
+
+        public Handler GetNext()
+        {
+            return this.next;
+        }
+
+        public abstract void HandleRequest(string request);
+    }
+}
